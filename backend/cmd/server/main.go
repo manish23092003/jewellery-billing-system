1ec9df5459ac0d68f9c3b1a23732d29e7d64599f
manish23092003 <manishsoni135790@gmail.com>
2026-06-24 19:52:19 +0530

    Allow all headers in CORS middleware

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -111,7 +111,7 @@ func main() {
 	app.Use(func(c *fiber.Ctx) error {
 		c.Set("Access-Control-Allow-Origin", "*")
 		c.Set("Access-Control-Allow-Methods", "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS")
-		c.Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization")
+		c.Set("Access-Control-Allow-Headers", "*")
 		if c.Method() == "OPTIONS" {
 			return c.SendStatus(fiber.StatusNoContent)
 		}
